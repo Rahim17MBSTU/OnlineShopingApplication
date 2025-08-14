367eeb8684c9eec1369ac109ee287973c923a223
Abdur Rahim <rahim125ak@gmail.com>
2025-08-14 13:48:39 +0600

    Create Order Controller

---
+++ OnlineShopingApplication/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopingApplication.Data;
+
+namespace OnlineShopingApplication.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
+    }
+}
